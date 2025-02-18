0abd733292a4b370cdf9d5af9b20ab86253ffbd7
fcedolini <fcedolini@gmail.com>
2025-02-18 20:08:24 -0300

    Primer commit

---
+++ proyecto_9.py
@@ -0,0 +1,59 @@
+import re
+import os
+import time
+import datetime
+from pathlib import Path
+import math
+
+inicio = time.time()
+
+# Modificar esta ruta con la ruta donde tengas tus directorios
+ruta = 'C:\\Users\\USUARIO\\Downloads\\Program\\CURSO-PY.2\\Python\\busca_num\\Mi_Gran_Directorio'
+
+# El patrón a buscar guardado en variable ej: NABC-12345; NXZ-98765
+mi_patron = r'N\D{3}-\d{5}'
+
+hoy = datetime.date.today()
+
+nros_encontrados = []
+archivos_encontrados = []
+
+
+def buscar_numero(archivo, patron):
+    este_archivo = open(archivo, 'r')
+    texto = este_archivo.read()
+    if re.search(patron, texto):
+        return re.search(patron, texto)
+    else:
+        return ''
+
+
+def crear_listas():
+    for carpeta, subcarpeta, archivo in os.walk(ruta):
+        for a in archivo:
+            resultado = buscar_numero(Path(carpeta, a), mi_patron)
+            if resultado != '':
+                nros_encontrados.append((resultado.group()))
+                archivos_encontrados.append(a.title())
+
+
+def mostrar_todo():
+    indice = 0
+    print('-' * 50)
+    print(f'Fecha de Búsqueda: {hoy.day}/{hoy.month}/{hoy.year}')
+    print('\n')
+    print('ARCHIVO\t\t\tNRO. SERIE')
+    print('-------\t\t\t----------')
+    for a in archivos_encontrados:
+        print(f'{a}\t{nros_encontrados[indice]}')
+        indice += 1
+    print('\n')
+    print(f'Números encotrados: {len(nros_encontrados)}')
+    fin = time.time()
+    duracion = fin - inicio
+    print(f'Duración de la búsqueda: {math.ceil(duracion)} segundos')
+    print('-' * 50)
+
+
+crear_listas()
+mostrar_todo()
